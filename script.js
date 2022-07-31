1976b4d94950905070626cad107e58c4cf818f32
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2022-07-31 13:06:34 -0500

    added some HTML elements, changed the CSS body property, and changed some text in the headings

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement("h2")
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "My name is James Salcedo, and I love coding!";
 
 document.querySelector("body").appendChild(h2);
 
