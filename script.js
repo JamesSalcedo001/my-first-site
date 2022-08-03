5918e3529d0b2a225d03a7132cbd642bac663d6b
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2022-08-03 01:28:32 -0500

    set images as buttons with click event

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ h2.textContent = "My name is James Salcedo, and I love coding!";
 
 document.querySelector("body").appendChild(h2);
 
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
